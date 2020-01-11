fc7d31348be3116ee9f11fb29bdbf6aa5a45aef4
Kina Rain <tina@lawtk.net>
2020-01-10 23:27:51 -0500

    uploading work into repository

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# noteTaker
-program to assist with taking notes
+# Note Taker
+This project was to make a program that will allow a user to take notes. 
+
+## Description
+
+This application allows a user to create, list, save and delete notes that are stored in a JSON file locally.
+
+This application will help users to keep track of their thoughts and after completing their to do list will be able to delete their tasks.
+
+## Business Context
+
+For users that need to keep track of a lot of information, it's easy to forget important information. Thus, being able to take and delete
+
+notes continuously will allow users to have written information available when needed.
+
+## Screen Shot
+
+*Link: https://https://github.com/Kina-rain/noteTaker
+
+![alt text][screenshot]
+
+[screenshot]: https://github.com/Kina-rain/noteTaker/blob/master/screenshot.png "Note Taker"
---
+++ server.js
@@ -0,0 +1,111 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//we had to set the static path of the assets directory so the server will find the css and js files
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Routes
+// ==============================================================
+
+// Home Route
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+// Notes Route
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"))
+})
+
+// Notes Get Route for raw data dump
+app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.join(__dirname, "/db/db.json"));
+  });
+
+// Notes Post Route
+app.post("/api/notes", function (req, res) {
+    
+    //get the user note from notes.html
+    var userNote = req.body;
+
+    //open up the database file and read in the contents
+    fs.readFile(path.join(__dirname, "db/db.json"), (err, data) => {
+        if (err) throw err;
+
+        //turn the data in the file into JSON so we can add the new note
+        let existingNotes = JSON.parse(data);
+
+        //get the maxID so we can increment it
+        let maxID = existingNotes[existingNotes.length - 1].id;
+
+        //make the newID a number
+        let newID = parseInt(maxID);
+
+        //increment the ID
+        newID++;
+
+        //set the id of the new note
+        userNote.id = newID.toString();
+
+        //Push the new note onto the array
+        existingNotes.push(userNote);
+
+        //write our file with the new data
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(existingNotes), function(err) {
+            if (err) throw err;
+        });
+    });
+
+    res.json(userNote);
+});
+
+// Notes Delete Route
+app.delete("/api/notes/:id", function(req, res) {
+
+    //get our noteID from the params so we know which one to delete
+    let noteID = req.params.id;
+
+    //get our file name
+    let filePath = path.join(__dirname, "db/db.json");
+
+    //open the db.json file so we can find the right entry to delete
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) throw err;
+
+        //get the data into our variable
+        let existingNotes = JSON.parse(data);
+
+        //loop through the data so we can find the right note to delete
+        for(i=0; i <= existingNotes.length; i++) {
+            //check to see if we have a match
+            if (existingNotes[i].id === noteID) {
+
+                //remove the existing element from the array using splice
+                existingNotes.splice(i, 1);
+
+                //write our file and end the res to close out the delete
+                fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(existingNotes), function(err) {
+                    if (err) throw err;
+                    return res.end();
+                });
+            }
+        }
+    });
+});
+
+//Start the server listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
